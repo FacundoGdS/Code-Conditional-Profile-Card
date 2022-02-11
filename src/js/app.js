1ebb6d4887d7810d68b43d62f71abd583ba64dcd
Facundo <facundogds@gmail.com>
2022-02-11 23:22:25 +0000

    Subo ejercicio completado

--- src/js/app.js
+++ src/js/app.js
@@ -31,19 +31,35 @@ function render(variables = {}) {
 
   // reset the website body with the new html output
   document.querySelector("#widget_content").innerHTML = `<div class="widget">
-            ${cover}
-          <img src="${variables.avatarURL}" class="photo" />
-          <h1>Lucy Boilett</h1>
-          <h2>Web Developer</h2>
-          <h3>Miami, USA</h3>
-          <ul class="position-right">
-            <li><a href="https://twitter.com/4geeksacademy"><i class="fab fa-twitter"></i></a></li>
-            <li><a href="https://github.com/4geeksacademy"><i class="fab fa-github"></i></a></li>
-            <li><a href="https://linkedin.com/4geeksacademy"><i class="fab fa-linkedin"></i></a></li>
-            <li><a href="https://instagram.com/4geeksacademy"><i class="fab fa-instagram"></i></a></li>
-          </ul>
-        </div>
-    `;
+  ${cover}
+<img src="${variables.avatarURL}" class="photo" />
+<h1>${variables.name ? variables.name : "Name"} ${
+    variables.lastname ? variables.lastname : "Last name"
+  }</h1>
+<h2>${variables.role ? variables.role : "Role"}</h2>
+<h3>${variables.city ? variables.city : "City"}, ${
+    variables.country ? variables.country : "Country"
+  }</h3>
+<ul class="${
+    variables.socialMediaPosition
+      ? variables.socialMediaPosition
+      : "position-right"
+  }">
+  <li><a href="${
+    variables.twitter
+      ? "https://twitter.com/" + variables.twitter
+      : "https://twitter.com/"
+  }"><i class="fab fa-twitter"></i></a></li>
+  <li><a href="${
+    variables.github === "alesanchezr"
+      ? "https://github.com/FacundoGdS"
+      : "https://github.com/4GeeksAcademy"
+  } "><i class="fab fa-github"></i></a></li>
+  <li><a href="https://www.linkedin.com/in/facundoguldossantos/"><i class="fab fa-linkedin"></i></a></li>
+  <li><a href="https://instagram.com/4geeksacademy"><i class="fab fa-instagram"></i></a></li>
+</ul>
+</div>
+`;
 }
 
 /**
@@ -54,9 +70,11 @@ window.onload = function() {
     // if includeCover is true the algorithm should
     includeCover: true,
     // this is the url of the image that will used as background for the profile cover
-    background: "https://images.unsplash.com/photo-1511974035430-5de47d3b95da",
+    background:
+      "https://www.nationalgeographic.com.es/medio/2018/02/27/playa-de-isuntza-lekeitio__1280x720.jpg",
     // this is the url for the profile avatar
-    avatarURL: "https://randomuser.me/api/portraits/women/42.jpg",
+    avatarURL:
+      "https://avatars.githubusercontent.com/u/92039939?s=400&u=7a18401aace9b09ab62e2ff0069655f04998a9ea&v=4",
     // social media bar position (left or right)
     socialMediaPosition: "position-left",
     // social media usernames
